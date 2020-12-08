9f368d586a3184437074f0d013fe65e990e722c4
Millie <47400469+Minnie-Million@users.noreply.github.com>
2020-12-08 14:58:13 -0800

    Tutorial 3.2.8 Complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -36,7 +36,6 @@ public class PlayerController : MonoBehaviour
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
-            Debug.Log("Game Over");
         }
     }
 }
